5528597165526d70bb3785eaf0a8b52295ed2587
ngovanvu203 <128632824+ngovanvu2003@users.noreply.github.com>
2023-07-27 06:55:36 +0700

    banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,86 @@
+
+import axios from "axios"
 import Banner from "@/components/Banner"
-export default function Home() {
+import requests from "@/api/movie"
+import { log } from "console"
+import Row from "@/components/Row"
+
+
+export default async function Home() {
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
+  ])
+  
   return (
    <>
-   <Banner/>
+   <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
+   <Banner netflixOriginals={netflixOriginals}/>
+   <section className="">
+    <Row/>
+    <Row/>
+
+    <Row/>
+
+    <Row/>
+    <Row/>
+
+    
+   </section>
+   </main>
    <h1 className=''>Hello</h1>
    </>
   )
 }
-export const getServerSideProps = () =>{
 
-}
+// export const getServerSideProps = async () => {
+//   const [
+//     netflixOriginals,
+//     trendingNow,
+//     topRated,
+//     actionMovies,
+//     comedyMovies,
+//     horrorMovies,
+//     romanceMovies,
+//     documentaries,
+//   ] = await Promise.all([
+//     fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+//     fetch(requests.fetchTrending).then((res) => res.json()),
+//     fetch(requests.fetchTopRated).then((res) => res.json()),
+//     fetch(requests.fetchActionMovies).then((res) => res.json()),
+//     fetch(requests.fetchComedyMovies).then((res) => res.json()),
+//     fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+//     fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+//     fetch(requests.fetchDocumentaries).then((res) => res.json()),
+//   ])
+//   return {
+//     props: {
+//       netflixOriginals: netflixOriginals.results,
+//       trendingNow: trendingNow.results,
+//       topRated: topRated.results,
+//       actionMovies: actionMovies.results,
+//       comedyMovies: comedyMovies.results,
+//       horrorMovies: horrorMovies.results,
+//       romanceMovies: romanceMovies.results,
+//       documentaries: documentaries.results,
+//     },   
+//   }
+// }
+
+
+
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,10 +1,41 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import { Movie } from "../../typings";
+import { baseUrl } from "@/api/constants";
+import {FaPlay } from 'react-icons/fa'
+import {GrCircleInformation} from 'react-icons/gr'
 
-const Banner = () => {
+
+type Props = {
+  netflixOriginals: Movie[] | any;
+};
+
+const Banner = ({ netflixOriginals }: Props) => {
+  const data = netflixOriginals.results;
+  const [movie, setMovie] = useState<Movie | null>(null);
+  useEffect(() => {
+    setMovie(data[Math.floor(Math.random() * data.length)]);
+  }, [data]);
+  // console.log(movie);
   return (
-    <div>
-      {/* <div><Image/></div> */}
+    <div className="flex flex-col space-x-2 py-16 md:space-y-4 md:h-[65vh] lg:justify-end">
+      <div className="absolute top-0 left-0 h-[95vh] -z-10 w-screen">
+        <Image
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path} `}
+          alt={"Picture of the author"}
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <h1 className="text-2xl  lg:text-7xl md:text-4xl">
+        {movie?.title || movie?.name || movie?.original_name}
+      </h1>
+      <p className="max-w-xs text-shadow-md text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">{movie?.overview}</p>
+      <div className="flex space-x-3">
+        <button className="buttonBanner bg-white text-black"><FaPlay className="h-4 w-4 text-black md:h-7 md:w-7"/>Xem ngay</button>
+        <button className="buttonBanner bg-[gray]/70 text-black"> <GrCircleInformation className="h-5 w-5 text-black md:h-8 md:w-8"/> Xem thông tin</button>
+      </div>
     </div>
   );
 };
